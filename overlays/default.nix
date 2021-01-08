081259ae504874a097d7a0647b9efeb996fbdf95
Max Verbeek <m4xv3rb33k@gmail.com>
2021-01-08 02:15:38 +0100

    Add hash that is easier to derive

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,7 +9,7 @@ let customOverlays = final: prev: {
       owner = "yshui";
       repo = "picom";
       rev = "d974367a0446f4f1939daaada7cb6bca84c893ef"; # most recent commit as of 2021-01-08
-      sha256 = "9b1eb469647ee21970307c0485848d5d97c77c6a20e5cd9a1e894cb609295556";
+      sha256 = "sha256-mx60aWR+4hlwMHwEhYSNXZfHfGog5c2aHolMtgkpVVY=";
       fetchSubmodules = true;
     };
   });
