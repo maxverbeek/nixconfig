73ff64c360b8a82db4d8b13d1b9fc664d4d9b692
Max Verbeek <m4xv3rb33k@gmail.com>
2021-02-08 12:40:17 +0100

    Add .desktop file for responsively

--- overlays/responsively-app/default.nix
+++ overlays/responsively-app/default.nix
@@ -1,9 +1,24 @@
 { lib, fetchurl, appimageTools }:
 
-appimageTools.wrapType2 {
-  name = "responsively";
+let
+  version = "0.15.0";
   src = fetchurl {
-    url = "https://github.com/responsively-org/responsively-app/releases/download/v0.15.0/ResponsivelyApp-0.15.0.AppImage";
+    url = "https://github.com/responsively-org/responsively-app/releases/download/v${version}/ResponsivelyApp-${version}.AppImage";
     sha256 = "sha256-/GUj2tjdAF6rfCwO49yhNPVjwOQazX5IX3BShcd5NYA=";
   };
+
+in
+
+appimageTools.wrapType2 {
+  name = "responsively";
+  src = src;
+  extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    echo "[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Exec=responsively
+Name=Responsively App
+Version=${version}" > $out/share/applications/responsively.desktop
+  '';
 }
