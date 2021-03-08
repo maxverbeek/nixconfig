4cafa81464a4418570ea69c84fde1ed3948eb17b
Max Verbeek <m4xv3rb33k@gmail.com>
2021-03-08 23:44:38 +0100

    Update icons to newer version

--- overlays/mcmojave-cursors/default.nix
+++ overlays/mcmojave-cursors/default.nix
@@ -8,7 +8,7 @@ stdenvNoCC.mkDerivation rec {
     owner = "vinceliuice";
     repo = "McMojave-cursors";
     rev = "master";
-    sha256 = "sha256-qmITQ5e83SA8CSD3YcGI+swGnmJXfXEAN+ecxLDWRkQ=";
+    sha256 = "sha256-4YqSucpxA7jsuJ9aADjJfKRPgPR89oq2l0T1N28+GV0=";
   };
 
   nativeBuildInputs = [ inkscape xcursorgen ];
