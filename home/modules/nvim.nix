cfab0f8f17c77ce3f1fa869478996b0f271af83c
Max Verbeek <m4xv3rb33k@gmail.com>
2021-07-05 21:30:19 +0200

    add lsp stuff

--- home/modules/nvim.nix
+++ home/modules/nvim.nix
@@ -36,10 +36,10 @@ let
     plugin = pkgs.vimPlugins.vim-easy-align;
     config = ''
       " Start interactive EasyAlign in visual mode (e.g. vipga)
-      xmap ga <Plug>(EasyAlign)
+      xmap <Leader>ga <Plug>(EasyAlign)
 
       " Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      nmap ga <Plug>(EasyAlign)
+      nmap <Leader>ga <Plug>(EasyAlign)
     '';
   };
 
@@ -118,6 +118,86 @@ let
     { parser = "yaml.so";       grammar = tree-sitter-yaml; }
   ];
 
+
+  nvim-lspconfig = {
+    plugin = pkgs.vimPlugins.nvim-lspconfig;
+    config = ''
+      filetype plugin indent on
+
+      " Set completeopt to have a better completion experience
+      " :help completeopt
+      " menuone: popup even when there's only one match
+      " noinsert: Do not insert text until a selection is made
+      " noselect: Do not select, force user to select one from the menu
+      set completeopt=menuone,noinsert,noselect
+
+      " Avoid showing extra messages when using completion
+      set shortmess+=c
+
+      " Always show signcolumn so that it doesnt (dis)appear when a new error happens
+      set signcolumn=yes
+
+      lua <<EOF
+
+      local nvim_lsp = require'lspconfig'
+
+      local on_attach = function(client)
+        require'completion'.on_attach(client)
+      end
+
+      -- Register all the language servers
+      nvim_lsp.rnix.setup { on_attach = on_attach }
+      nvim_lsp.tsserver.setup { on_attach = on_attach }
+      nvim_lsp.rust_analyzer.setup { on_attach = on_attach }
+
+      -- Enable diagnostics
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+          virtual_text = true,
+          signs = true,
+          update_in_insert = true,
+        }
+      )
+
+      EOF
+
+      " Code navigation shortcuts
+      nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
+      nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
+      nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
+      nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+      nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
+      nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+      nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
+      nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+      nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.declaration()<CR>
+
+      nnoremap <silent> ga    <cmd>lua vim.lsp.buf.code_action()<CR>
+
+      " Use <Tab> and <S-Tab> to navigate through popup menu if it's open
+      inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+      inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+
+      " use <Tab> as trigger keys
+      imap <Tab> <Plug>(completion_smart_tab)
+      imap <S-Tab> <Plug>(completion_smart_s_tab)
+
+      " Set updatetime for CursorHold
+      " 300ms of no cursor movement to trigger CursorHold
+      set updatetime=300
+
+      " Show diagnostic popup on cursor hold
+      autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
+
+      " Goto previous/next diagnostic warning/error
+      nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
+      nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+
+      autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *
+      \ lua require'lsp_extensions'.inlay_hints{ prefix = "", highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
+    '';
+  };
+
 in {
   # add treesitter grammars
   home.file =
@@ -140,14 +220,25 @@ in {
     vimAlias = true;
     vimdiffAlias = true;
 
+    extraPackages = with pkgs; [
+      rnix-lsp
+      nodePackages.typescript-language-server
+      rust-analyzer
+    ];
+
     plugins = with pkgs.vimPlugins; [
       # plugins with config (see above)
       colorscheme
       fzf
       easyAlign
       ultisnips
+
       treesitter
       nvim-treesitter-textobjects # config is inside of treesitter
+
+      nvim-lspconfig
+      lsp_extensions-nvim # config inside of nvim-lspconfig
+      completion-nvim # config inside of nvim-lspconfig
 
       # other plugins
       vim-nix
