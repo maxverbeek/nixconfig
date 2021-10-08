80c98147e9ae66d5dad9b7ea7c691f122bdaba94
Max Verbeek <m4xv3rb33k@gmail.com>
2021-10-09 01:00:25 +0200

    change autocomplete key to tab

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -172,7 +172,7 @@ let
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.close(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping.confirm({ select = true }),
           },
           sources = {
             { name = 'nvim_lsp' },
