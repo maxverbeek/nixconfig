8b4a442afbc4604db4233df5cf883210b726b211
Max Verbeek <m4xv3rb33k@gmail.com>
2022-01-05 01:31:53 +0100

    fix: dont rebuild cursors every time

--- packages/mcmojave-cursors/default.nix
+++ packages/mcmojave-cursors/default.nix
@@ -7,16 +7,9 @@ stdenvNoCC.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = "McMojave-cursors";
-    rev = "master";
+    rev = "7d0bfc1f91028191cdc220b87fd335a235ee4439";
     sha256 = "sha256-4YqSucpxA7jsuJ9aADjJfKRPgPR89oq2l0T1N28+GV0=";
   };
-
-  nativeBuildInputs = [ inkscape xcursorgen ];
-
-  buildPhase = ''
-    patchShebangs build.sh
-    ./build.sh
-  '';
   
   installPhase = ''
     install -dm 0755 $out/share/icons/McMojave-cursors
