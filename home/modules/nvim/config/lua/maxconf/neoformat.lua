6ca5a145594c6a36b916e9a36135c69427e88155
Max Verbeek <m4xv3rb33k@gmail.com>
2022-09-13 23:18:20 +0200

    feat: add toggle for autoformatting

--- home/modules/nvim/config/lua/maxconf/neoformat.lua
+++ home/modules/nvim/config/lua/maxconf/neoformat.lua
@@ -1,12 +1,7 @@
 vim.cmd [[
-    augroup fmt
-      autocmd!
-      autocmd BufWritePre * Neoformat
-    augroup END
-
     let g:neoformat_proto_clangformat = {
     \ 'exe': 'clang-format',
-    \ 'args': ['--style=google --assume-filename=file.proto'],
+    \ 'args': ['--style=google --assume-filename="%:p"'],
     \ 'no_append': 1,
     \ 'stdin': 1,
     \ }
@@ -36,3 +31,26 @@ vim.cmd [[
 local opt = { noremap = true, silent = false }
 vim.api.nvim_set_keymap('n', '<Leader>ff', '<CMD>Neoformat<cr>', opt)
 vim.api.nvim_set_keymap('x', '<Leader>ff', '<CMD>Neoformat!<cr>', opt)
+
+local augroup = vim.api.nvim_create_augroup("FormatOnSave", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup,
+    command = 'Neoformat',
+})
+
+local toggleAutoformat = function()
+    if #vim.api.nvim_get_autocmds({ group = augroup }) > 0 then
+        vim.api.nvim_clear_autocmds({ group = augroup })
+        print("Disabled autoformatting")
+    else
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            command = 'Neoformat',
+        })
+        print("Enabled autoformatting")
+    end
+end
+
+vim.api.nvim_create_user_command("ToggleFormatting", toggleAutoformat, {})
+vim.keymap.set('n', '<F4>', toggleAutoformat)
