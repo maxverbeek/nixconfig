5e6d6a2c0f19455b1cb4be31a250fb82fc606ce4
Max Verbeek <m4xv3rb33k@gmail.com>
2023-03-15 17:37:23 +0100

    feat: update vimtex config

--- home/modules/nvim/config/ftplugin/tex.vim
+++ home/modules/nvim/config/ftplugin/tex.vim
@@ -1,3 +1,4 @@
 let g:vimtex_view_method = 'zathura'
+let g:vimtex_quickfix_open_on_warning = 0
 
 let maplocalleader = " "
--- home/modules/nvim/config/lua/maxconf/treesitter.lua
+++ home/modules/nvim/config/lua/maxconf/treesitter.lua
@@ -1,61 +1,61 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
 
-    disable = {
-        "cuda",
-        "cpp",
-    },
-  },
+		disable = {
+			"cuda",
+			"cpp",
+			"latex",
+		},
+	},
 
-  indent = {
-    enable = true,
-  },
+	indent = {
+		enable = true,
+	},
 
-  -- requires nvim-autopairs
-  autopairs = {
-    enable = true,
-  },
+	-- requires nvim-autopairs
+	autopairs = {
+		enable = true,
+	},
 
-  -- requires nvim-ts-autotag
-  autotag = {
-    enable = true,
-  },
+	-- requires nvim-ts-autotag
+	autotag = {
+		enable = true,
+	},
 
-  -- requires nvim-treesitter-textobjects
-  textobjects = {
-    select = {
-      enable = true,
+	-- requires nvim-treesitter-textobjects
+	textobjects = {
+		select = {
+			enable = true,
 
-      lookahead = true,
+			lookahead = true,
 
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
 
-        ["ac"] = "@conditional.outer",
-        ["ic"] = "@conditional.inner",
+				["ac"] = "@conditional.outer",
+				["ic"] = "@conditional.inner",
 
-        ["iC"] = "@class.inner",
-        ["aC"] = "@class.outer",
+				["iC"] = "@class.inner",
+				["aC"] = "@class.outer",
 
-        ["iB"] = "@block.inner",
-        ["aB"] = "@block.outer",
+				["iB"] = "@block.inner",
+				["aB"] = "@block.outer",
 
-        -- argument (p is already paragraph)
-        ["ia"] = "@parameter.inner",
-        ["aa"] = "@parameter.outer",
+				-- argument (p is already paragraph)
+				["ia"] = "@parameter.inner",
+				["aa"] = "@parameter.outer",
 
-        -- invocation
-        ["ii"] = "@call.inner",
-        ["ai"] = "@call.outer",
-      },
-    },
-  },
+				-- invocation
+				["ii"] = "@call.inner",
+				["ai"] = "@call.outer",
+			},
+		},
+	},
 
-  -- requires playground
-  playground = {
-    enable = true,
-  }
-}
-
+	-- requires playground
+	playground = {
+		enable = true,
+	},
+})
