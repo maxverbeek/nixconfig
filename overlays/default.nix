eb078a8d94db15d9ccf8ccf137645c77152b443e
Max Verbeek <m4xv3rb33k@gmail.com>
2023-07-24 18:08:48 +0200

    chore: add debug flag to zsh for tracing

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,11 @@
-final: prev:
-{
+final: prev: {
   # use newer version of picom
   picom = prev.picom.overrideAttrs (oldAttrs: {
     src = prev.fetchFromGitHub {
       owner = "yshui";
       repo = "picom";
-      rev = "d974367a0446f4f1939daaada7cb6bca84c893ef"; # most recent commit as of 2021-01-08
+      rev =
+        "d974367a0446f4f1939daaada7cb6bca84c893ef"; # most recent commit as of 2021-01-08
       sha256 = "sha256-mx60aWR+4hlwMHwEhYSNXZfHfGog5c2aHolMtgkpVVY=";
       fetchSubmodules = true;
     };
@@ -17,7 +17,8 @@ final: prev:
     src = prev.fetchFromGitLab {
       owner = "maxverbeek";
       repo = "xidlehook";
-      rev = "adf2758f51cbf1883387db852345498160f88a92"; # master as of 2021-11-14
+      rev =
+        "adf2758f51cbf1883387db852345498160f88a92"; # master as of 2021-11-14
       sha256 = "sha256-Sk4xMzMVLmgrelkaghuv5HAMx5YdgaDU+VqXwWG+9i8=";
     };
 
@@ -26,4 +27,8 @@ final: prev:
       outputHash = "sha256-Iuri3dOLzrfTzHvwOKcZrVJFotqrGlM6EeuV29yqz+U=";
     });
   });
+
+  # zsh = prev.zsh.overrideAttrs (oldAttrs: {
+  #   configureFlags = [ "--enable-zsh-debug" ] ++ oldAttrs.configureFlags;
+  # });
 }
