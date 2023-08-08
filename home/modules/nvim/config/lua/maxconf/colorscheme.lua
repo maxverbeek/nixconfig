73246137e6393f393ca156b847d964ff790da727
Max Verbeek <m4xv3rb33k@gmail.com>
2023-08-08 12:44:34 +0200

    feat: highlight trailing whitespace

--- home/modules/nvim/config/lua/maxconf/colorscheme.lua
+++ home/modules/nvim/config/lua/maxconf/colorscheme.lua
@@ -1,12 +1,14 @@
-local default_colors = require('kanagawa.colors').setup()
+local default_colors = require("kanagawa.colors").setup()
 
 local overrides = {
   -- make the indent line of the current context a gray-ish color
-  IndentBlanklineContextChar = { link = "IndentBlanklineChar" }
+  IndentBlanklineContextChar = { link = "IndentBlanklineChar" },
+  TrailingWhitespace = { link = "DiffDelete" },
 }
 
-require('kanagawa').setup({
-  overrides = overrides
+require("kanagawa").setup({
+  overrides = overrides,
 })
 
 vim.cmd("colorscheme kanagawa")
+vim.cmd("match TrailingWhitespace /\\s\\+$/")
