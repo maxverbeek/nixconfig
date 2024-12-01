044cac7c5b12eb72a36078975a71eb99bd8ecdae
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-01 12:43:09 +0100

    feat: fix bluetooth on nixos 24.11

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -10,20 +10,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     jack.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+          ];
+        };
+      };
+    };
   };
-
-  # enable better codecs for bluetooth headset
-  # environment.etc = {
-  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-  #     bluez_monitor.properties = {
-  #       ["bluez5.enable-sbc-xq"] = true,
-  #       ["bluez5.enable-msbc"] = true,
-  #       ["bluez5.enable-hw-volume"] = true,
-  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-  #     }
-  #   '';
-  # };
 
   hardware.pulseaudio.enable = lib.mkForce false;
 }
