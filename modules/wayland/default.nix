7dd0a97e8bba1b06531894c2a08e5fb280feee02
Max Verbeek <m4xv3rb33k@gmail.com>
2025-01-13 16:38:23 +0100

    feat: update

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -21,7 +21,9 @@
 
   # testing!
   modules.niri.enable = true;
-  modules.niri.package = pkgs.unstable.niri.override { mesa = pkgs.mesa; };
+  modules.niri.package = pkgs.unstable.niri.overrideAttrs (old: {
+    buildInputs = ((builtins.filter (p: p != pkgs.unstable.libgbm) old.buildInputs) ++ [ pkgs.mesa ]);
+  });
 
   modules.xwayland-satellite = {
     enable = true;
