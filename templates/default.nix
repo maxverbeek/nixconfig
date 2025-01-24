ecb707777644f171bb28b4c5bf239be5b161b9a7
Max Verbeek <m4xv3rb33k@gmail.com>
2025-01-25 00:19:56 +0100

    feat: add python template

--- templates/default.nix
+++ templates/default.nix
@@ -8,4 +8,9 @@
     description = "Rust development environment using an overlay";
     path = ./rust;
   };
+
+  python = {
+    description = "Python development with pip in a venv (needs nix-ld for C packages)";
+    path = ./python;
+  };
 }
