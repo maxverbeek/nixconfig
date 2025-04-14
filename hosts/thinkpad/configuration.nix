df9c8aa87ecee5ae23464d168f161f4425aaef2c
Max Verbeek <m4xv3rb33k@gmail.com>
2025-04-14 21:28:10 +0200

    fix: enable light program

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -110,6 +110,9 @@
   services.upower.enable = true;
   services.gvfs.enable = true;
 
+  # monitoring brightness
+  programs.light.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.max = {
     isNormalUser = true;
