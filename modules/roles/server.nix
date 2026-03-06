a0a1959dcd889d34797340c1c0188962c822131b
Max Verbeek <m4xv3rb33k@gmail.com>
2026-03-06 09:54:12 +0100

    feat: add fail2ban

--- modules/roles/server.nix
+++ modules/roles/server.nix
@@ -13,5 +13,10 @@
       users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBmyftE9tuFUn/8m03M6aS0okxA7B1QFBxZNhP4CZ8F"
       ];
+
+      services.fail2ban = {
+        enable = true;
+        bantime = "${toString (7 * 24)}h";
+      };
     };
 }
